3a905c411b87f1a8ce4f0fba1238950f2f26f552
fse-dev <elmanoukdesign@gmail.com>
2020-06-07 03:25:44 -0400

    adjusted the crossing moving avg according to the high volilatity

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Project description:
 
-- This project is an emerging crypto trading option that allows anyone to automate their trading activities. Signal incorporates a number of trading options including regular market and limit buy and sell orders, as well as simultaneous stop-loss/take-profit orders, and trailing stop-loss orders. In this project you can get last price and get signal by using a bot. It s available on Telegram for external testing. Currently i am applying **Crossing Simple Moving Average Strategy.** Later i m planning to add more feature regarding to financial strategies.
+- This project is an emerging crypto trading option that allows anyone to automate their trading activities. Signal incorporates a number of trading options including regular market and limit buy and sell orders, as well as simultaneous stop-loss/take-profit orders, and trailing stop-loss orders. In this project you can get last price and get signal by using a bot. It s available on Telegram for external testing. Currently i am applying **Crossing Simple Moving Average Strategy.(5, 8 and 13 )** Later i m planning to add more feature regarding to financial strategies.
 
 ## Dev Depencencies
 
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 import requests
 from collections import Counter
-from settings import SECRET_KEY,APP_HASH,API_ID,BOT_NAME,ROOM_ID
+from settings import SECRET_KEY, APP_HASH, API_ID, BOT_NAME, ROOM_ID
 from telethon import TelegramClient, events
 
 client = TelegramClient(BOT_NAME, API_ID, APP_HASH)
@@ -11,16 +11,16 @@ ENDPOINT = "https://query1.finance.yahoo.com/v8/finance/chart/"
 
 # Stock symbol
 
+
 def createURL(sym, ran, inter):
     return ENDPOINT+sym+"?"+"range="+ran+"&interval="+inter
 
 
-
-def sendMessage(_token, _id, _message): 
-    url = 'https://api.telegram.org/bot'+_token+'/sendMessage?chat_id='+_id+'&text='+_message
+def sendMessage(_token, _id, _message):
+    url = 'https://api.telegram.org/bot'+_token + \
+        '/sendMessage?chat_id='+_id+'&text='+_message
     res = requests.post(url)
     return res
-
 
 
 url = createURL("BTC-USD", "1d", "2m")
@@ -29,7 +29,7 @@ _rawcolumn_names = list(req['chart']['result'][0])
 values = req['chart']['result'][0][_rawcolumn_names[2]]['quote'][0]
 cols = list(values.keys())
 
-# Create Dataframe 
+# Create Dataframe
 df = pd.DataFrame(columns=cols)
 df[cols[0]] = values['close']
 df[cols[1]] = values['high']
@@ -37,53 +37,91 @@ df[cols[2]] = values['open']
 df[cols[3]] = values['volume']
 df[cols[4]] = values['low']
 
-sma_9 = pd.DataFrame()
-sma_9['Close Price'] = df['close'].rolling(window= 9).mean().dropna()
-sma_14 = pd.DataFrame()
-sma_14['Close Price'] = df['close'].rolling(window= 14).mean().dropna()
+sma_5 = pd.DataFrame()
+sma_5['Close Price'] = df['close'].rolling(window=5).mean().dropna()
+sma_8 = pd.DataFrame()
+sma_8['Close Price'] = df['close'].rolling(window=8).mean().dropna()
+sma_13 = pd.DataFrame()
+sma_13['Close Price'] = df['close'].rolling(window=13).mean().dropna()
 
 data = pd.DataFrame()
 data['BTC-USD'] = df['close']
-data['SMA 9'] = sma_9['Close Price']
-data['SMA 14'] = sma_14['Close Price']
-price = data['BTC-USD'].tail(5)
+data['SMA 5'] = sma_5['Close Price']
+data['SMA 8'] = sma_8['Close Price']
+data['SMA 13'] = sma_13['Close Price']
+
 
 def create_signal(data):
     buy_sig_price = []
     sell_sig_price = []
     flag = -1
-    
+
     for i in range(len(data)):
-        if data['SMA 9'][i] > data['SMA 14'][i]:
-            
+        if data['SMA 5'][i] > data['SMA 8'][i]:
             if flag != 1:
-                
+
                 buy_sig_price.append(data["BTC-USD"][i])
                 sell_sig_price.append(np.nan)
                 flag = 1
-                
             else:
                 buy_sig_price.append(np.nan)
                 sell_sig_price.append(np.nan)
-                
-        elif data['SMA 9'][i] < data['SMA 14'][i]:
-            
+
+        elif data['SMA 5'][i] < data['SMA 8'][i]:
             if flag != 0:
-                    
                 buy_sig_price.append(np.nan)
                 sell_sig_price.append(data["BTC-USD"][i])
                 flag = 0
-                
             else:
-                
                 buy_sig_price.append(np.nan)
                 sell_sig_price.append(np.nan)
-                
+
+        elif data['SMA 8'][i] > data['SMA 13'][i]:
+            if flag != 1:
+                buy_sig_price.append(data["BTC-USD"][i])
+                sell_sig_price.append(np.nan)
+                flag = 1
+
+            else:
+
+                buy_sig_price.append(np.nan)
+                sell_sig_price.append(np.nan)
+        elif data['SMA 8'][i] < data['SMA 13'][i]:
+            if flag != 0:
+                buy_sig_price.append(np.nan)
+                sell_sig_price.append(data["BTC-USD"][i])
+                flag = 0
+
+            else:
+
+                buy_sig_price.append(np.nan)
+                sell_sig_price.append(np.nan)
+        elif data['SMA 5'][i] > data['SMA 13'][i]:
+            if flag != 1:
+                buy_sig_price.append(data["BTC-USD"][i])
+                sell_sig_price.append(np.nan)
+                flag = 1
+
+            else:
+
+                buy_sig_price.append(np.nan)
+                sell_sig_price.append(np.nan)
+        elif data['SMA 5'][i] < data['SMA 13'][i]:
+            if flag != 0:
+                buy_sig_price.append(np.nan)
+                sell_sig_price.append(data["BTC-USD"][i])
+                flag = 0
+
+            else:
+
+                buy_sig_price.append(np.nan)
+                sell_sig_price.append(np.nan)
+
         else:
-            
+
             buy_sig_price.append(np.nan)
             sell_sig_price.append(np.nan)
-                
+
     return (buy_sig_price, sell_sig_price)
 
 
@@ -92,44 +130,49 @@ def create_signal(data):
 buy_sell = create_signal(data)
 data["Buy Signal Price"] = buy_sell[0]
 data["Sell Signal Price"] = buy_sell[1]
+buy_price = data["Buy Signal Price"].dropna().values.astype(float)
+sell_price = data["Sell Signal Price"].dropna().values.astype(float)
 
-#show data
-B = [i for i in data["Buy Signal Price"].dropna()]
-S = [i for i in data["Sell Signal Price"].dropna()]
 
-arr = []
 def validateBS():
-    for i in range(0,20):
-        if   S[i] > B[i] :
-            arr.append("BUY")
-        else:
-            arr.append("SELL")    
-validateBS()
+    arr = []
 
+    for i in range(0, 23):
+        if buy_price[i] < sell_price[i]:
+            arr.append(1)
+        else:
+            arr.append(0)
+
+    return arr
 
 
 def results():
-    data = dict(Counter(arr))
-    names = list(data.keys())
-    values = list(data.items())
-    msg = "BUY" if data["BUY"] < data["SELL"] else "NETURAL" if data["BUY"] == data["SELL"] else "SELL"  
+    arr = validateBS()
+    if arr.count(True) < arr.count(False):
+        msg = "Sell"
+    else:
+        msg = "Buy"
+
     return msg
 
-    
+
+# results()
 @client.on(events.NewMessage(chats=("testSite")))
 async def handle_signal_req(event):
+    # await event.reply(sendMessage(SECRET_KEY, ROOM_ID, results()))
     if 'signal' in event.raw_text or 'Signal' in event.raw_text or 'SIGNAL' in event.raw_text:
         await event.reply(sendMessage(SECRET_KEY, ROOM_ID, results()))
 
+
 @client.on(events.NewMessage(chats=("testSite")))
 async def handle_price_req(event):
-    
+    price = str(data['BTC-USD'].tail(1).dropna().values.astype(float)[0])
     if 'price' in event.raw_text or 'Price' in event.raw_text or 'PRICE' in event.raw_text:
-        for i in price.dropna():
-            await event.reply( sendMessage(
-                SECRET_KEY, ROOM_ID, str(i)
-            ))
-        
+        await event.reply(sendMessage(
+            SECRET_KEY,
+            ROOM_ID,
+            price
+        ))
 
 client.start()
 client.run_until_disconnected()
